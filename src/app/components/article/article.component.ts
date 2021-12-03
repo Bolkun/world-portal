94bb06b4aa153296b020851d4231e599c9fcf2fb
Hamedkabir <38732428+Hamedkabir@users.noreply.github.com>
2021-12-03 19:58:35 +0100

    close button für modal artikel

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-article',
@@ -25,9 +25,12 @@ export class ArticleComponent implements OnInit {
       createdAt: '03.12.2021'
     }
   ]
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private modalCtl: MatDialog) { }
 
   ngOnInit(): void { }
 
+  closeArticle() {
+    this.modalCtl.closeAll();
+  }
 }
 
