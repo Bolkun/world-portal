777d4078398ad759011f4f7fbe243898153651e0
Hamedkabir <38732428+Hamedkabir@users.noreply.github.com>
2021-12-03 19:24:44 +0100

    article component grid system

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -7,9 +7,27 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
+  commentMockUps = [
+    {
+      name: 'Hamedkabir',
+      comment: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam',
+      createdAt: '03.12.2021'
+    }
+    ,
+    {
+      name: 'Hamedkabir',
+      comment: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam',
+      createdAt: '03.12.2021'
+    },
+    {
+      name: 'Hamedkabir',
+      comment: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam',
+      createdAt: '03.12.2021'
+    }
+  ]
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
 }
 
